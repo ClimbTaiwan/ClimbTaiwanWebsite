ed73ad3a34d7645d82e4ce623a0cd75c53f8320f
Luke G <lukegronert@gmail.com>
2021-04-26 14:37:15 +0800

    Changed logo in navbar. Changed navlinks to black. Edited Crag name to have a bar between english and chinese names.

--- src/renderCragCard.js
+++ src/renderCragCard.js
@@ -118,7 +118,7 @@ const createCragCard = function(crag) {
     cragImg.setAttribute("src", crag.imgURL);
 
     if(crag.name != "Information") {
-      const cragNameText = document.createTextNode(crag.name   + ' (' + crag.chineseName + ')');
+      const cragNameText = document.createTextNode(crag.name   +' │ ' + crag.chineseName);
       cragName.appendChild(cragNameText);
     } else {
       const cragNameText = document.createTextNode(crag.h1);
